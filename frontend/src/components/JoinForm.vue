f507b1970c82b80ce581def085be81e5544a5045
Izoret <izoret@proton.me>
2026-08-09 15:26:31 +0200

    aesthetics + room code in uppercase css

--- frontend/src/components/JoinForm.vue
+++ frontend/src/components/JoinForm.vue
@@ -1,26 +1,37 @@
 <script setup>
-import { store, computedStore } from '../store.js';
-import { useWebSocket } from '../composables/useWebSocket.js';
-const { connect, sendMessage } = useWebSocket();
+import {store, computedStore} from '../store.js'
+import {useWebSocket} from '../composables/useWebSocket.js'
+
+const {connect, sendMessage} = useWebSocket();
 
 const joinRoom = async () => {
-    if (!computedStore.canJoin.value) return;
+    if (!computedStore.canJoin.value) return
     try {
-        await connect();
+        await connect()
         sendMessage('join_room', {
             playerName: store.playerName,
             roomCode: store.roomCode.toUpperCase(),
-        });
+        })
     } catch (err) {
-        store.errorMessage = 'Impossible de se connecter au serveur.';
+        store.errorMessage = 'Impossible de se connecter au serveur.'
     }
-};
+}
 </script>
 
 <template>
-  <div class="join-form">
-    <input type="text" v-model="store.playerName" placeholder="Votre nom" />
-    <input type="text" v-model="store.roomCode" placeholder="Code du salon" @keyup.enter="joinRoom" />
-    <button @click="joinRoom" :disabled="!computedStore.canJoin.value">Rejoindre le salon</button>
-  </div>
+    <div id="join-form">
+        <input type="text" v-model="store.playerName" placeholder="Pseudo"/>
+        <input type="text" v-model="store.roomCode" placeholder="Code du salon" @keyup.enter="joinRoom" id="room-code"/>
+        <button @click="joinRoom" :disabled="!computedStore.canJoin.value">Rejoindre/Créer salon</button>
+    </div>
 </template>
+
+<style scoped>
+#room-code {
+    text-transform: uppercase;
+
+    &::placeholder {
+        text-transform: none;
+    }
+}
+</style>
